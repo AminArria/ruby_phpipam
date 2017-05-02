60c6be239bc0b3b23a154e755aadd5ac24942d1c
Amin Arria <arria.amin@gmail.com>
2017-05-02 18:21:40 -0400

    Configuration

--- lib/phpipam.rb
+++ lib/phpipam.rb
@@ -1,5 +1,13 @@
 require "phpipam/version"
+require "phpipam/configuration"
 
 module Phpipam
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
 end
---
+++ lib/phpipam/configuration.rb
@@ -0,0 +1,8 @@
+module Phpipam
+  class Configuration
+    attr_accessor :base_url, :username, :password
+
+    def initialize
+    end
+  end
+end
