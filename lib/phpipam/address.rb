a019b1031f2e0a5f6cb40ca59ff4362220a3363e
Amin Arria <arria.amin@gmail.com>
2017-05-10 11:58:22 -0400

    Class Address

---
+++ lib/phpipam/address.rb
@@ -0,0 +1,37 @@
+module Phpipam
+  class Address
+    attr_reader :id, :subnetId, :ip, :is_gateway, :description, :hostname,
+                :mac, :owner, :tag, :PTRignore, :PTR, :deviceId, :port, :note,
+                :lastSeen, :excludePing, :editDate
+
+    def initialize(json)
+      @id = json[:id].to_i
+      @subnetId = json[:subnetId].to_i
+      @ip = json[:ip]
+      @is_gateway = json[:is_gateway] == "0" ? false : true
+      @description = json[:description]
+      @hostname = json[:hostname]
+      @mac = json[:mac]
+      @owner = json[:owner]
+      @tag = json[:tag].to_i
+      @PTRignore = json[:PTRignore] == "0" ? false : true
+      @PTR = json[:PTR].to_i
+      @deviceId = json[:deviceId].to_i
+      @port = json[:port]
+      @note = json[:note]
+      @lastSeen = json[:lastSeen] == "0000-00-00 00:00:00" ? nil : Time.strptime(json[:lastSeen], '%Y-%m-%d %H:%M:%S')
+      @excludePing = json[:excludePing] == "0" ? false : true
+      @editDate = Time.strptime(json[:editDate], '%Y-%m-%d %H:%M:%S')
+    end
+
+    def self.get(id)
+      response = HTTParty.get(Phpipam.gen_url("/addresses/#{id}/"),
+          headers: {token: Phpipam.token}
+        )
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      Address.new(body[:data])
+    end
+  end
+end
