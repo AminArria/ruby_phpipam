cfe53ef68a2d1ba1018953052d5a5b5524d680f6
Amin Arria <arria.amin@gmail.com>
2017-06-27 13:13:57 -0400

    Address specs

---
+++ spec/ruby_phpipam/address_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+RSpec.describe RubyPhpipam::Address do
+  before :each do
+    load_configuration
+    RubyPhpipam.authenticate
+  end
+
+  it "raises an error when address doesn't exist", :vcr do
+    expect {
+      RubyPhpipam::Address.get(999)
+    }.to raise_error(RubyPhpipam::RequestFailed)
+  end
+
+  it 'gets a address by id', :vcr do
+    address = RubyPhpipam::Address.get(9)
+    expect(address.id).to eq 9
+  end
+end
